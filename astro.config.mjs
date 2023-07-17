0716d47e12e3af57745a0ad728a9974d303e43b6
Parijat Khan <pkhan@mountainleverage.com>
2023-07-18 07:48:16 +1200

    Added initial layout and research page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), image(), mdx()]
 });
